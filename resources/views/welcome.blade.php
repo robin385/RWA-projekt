0164b671f990f4d7e20d2db0ca8babf237d768af
patrikMedur01 <84334238+patrikMedur01@users.noreply.github.com>
2021-12-27 15:39:14 +0100

    robine prestani

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,6 @@
     @guest
         <x-login-register></x-login-register>
     @endguest
-<<<<<<< HEAD
     <!-- ja sam robin i ja patrik -->
-=======
-    <!-- ja sam robin  aaaaa neeee -->
->>>>>>> d2b93b6c6874deedefd1179f31d40add28864a04
+
 </x-layout>
